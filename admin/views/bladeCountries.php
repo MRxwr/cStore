f9974811d7c44446bdfd495e9bf62f68cdc68435
MRxwr <nasserhatab@gmail.com>
2023-11-05 01:28:21 +0300

    Update bladeCountries.php

--- admin/views/bladeCountries.php
+++ admin/views/bladeCountries.php
@@ -10,6 +10,12 @@ if ( isset($_GET['idon']) ){
 <div class="row">
 <div class="col-sm-12">
 <div class="panel panel-default card-view">
+<div class="panel-heading">
+<div class="pull-left">
+<h6 class="panel-title txt-dark"><?php echo direction("List of Countries","قائمة الدول") ?></h6>
+</div>
+<div class="clearfix"></div>
+</div>
 <div class="panel-wrapper collapse in">
 <div class="panel-body row">
 <div class="table-wrap">
