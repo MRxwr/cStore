1091220ca7814bd22f64d357e01145b80ebb6bec
MRxwr <nasserhatab@gmail.com>
2023-11-05 02:42:46 +0300

    Reports

--- admin/views/bladeReports.php
+++ admin/views/bladeReports.php
@@ -1,41 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<?php require("template/header.php");
+<?php 
+require("template/header.php");
 ?>
-
-	<body>
-		<!--Preloader-->
-		<div class="preloader-it">
-			<div class="la-anim-1"></div>
-		</div>
-		<!--/Preloader-->
-		<div class="wrapper  theme-1-active pimary-color-green">
-			
-			<!-- Top Menu Items -->
-		<?php require ("template/navbar.php") ?>
-		<!-- /Top Menu Items -->
-		
-		<!-- Left Sidebar Menu -->
-		<?php require("template/leftSideBar.php") ?>
-		<!-- /Left Sidebar Menu -->
-			
-			<!-- Right Sidebar Backdrop -->
-			<div class="right-sidebar-backdrop"></div>
-			<!-- /Right Sidebar Backdrop -->
-			
-			<!-- Main Content -->
-			<div class="page-wrapper">
-				<div class="container-fluid">
-					<!-- Title -->
-					<div class="row heading-bg">
-						<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-						  <h5 class="txt-dark">Reports</h5>
-						</div>
-					</div>
-					<!-- /Title -->
-					
-					<!-- Row -->
-					<style>
+<div class="row heading-bg">
+	<div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+		<h5 class="txt-dark">Reports</h5>
+	</div>
+</div>
+<style>
 [type="date"] {
   background:#fff url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/calendar_2.png)  97% 50% no-repeat ;
 }
@@ -341,116 +312,58 @@ $totalIntInvoices = 0;
 ?>
 <!-- /Row -->
 </div>
+</div>
 
-					<!-- /Row -->
-
-				</div>
-				
-				<!-- Footer -->
-			<?php require("template/footer.php") ?>
-			<!-- /Footer -->
-				
-			</div>
-			<!-- /Main Content -->
-		
-		</div>
-		<!-- /#wrapper -->
-		
-		<!-- JavaScript -->
-		
-		<!-- jQuery -->
-		<script src="../vendors/bower_components/jquery/dist/jquery.min.js"></script>
-		
-		<!-- Bootstrap Core JavaScript -->
-		<script src="../vendors/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-		
-		<!-- Data table JavaScript -->
-	<script src="../vendors/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
-	<script src="../vendors/bower_components/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-	<script src="../vendors/bower_components/datatables.net-buttons/js/buttons.flash.min.js"></script>
-	<script src="../vendors/bower_components/jszip/dist/jszip.min.js"></script>
-	<script src="../vendors/bower_components/pdfmake/build/pdfmake.min.js"></script>
-	<script src="../vendors/bower_components/pdfmake/build/vfs_fonts.js"></script>
-	
-	<script src="../vendors/bower_components/datatables.net-buttons/js/buttons.html5.min.js"></script>
-	<script src="../vendors/bower_components/datatables.net-buttons/js/buttons.print.min.js"></script>
-	<script src="dist/js/export-table-data.js"></script>
-	
-		<!-- Switchery JavaScript -->
-		<script src="../vendors/bower_components/switchery/dist/switchery.min.js"></script>
-		
-		<!-- Bootstrap Select JavaScript -->
-		<script src="../vendors/bower_components/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-		
-		<!-- Bootstrap Datetimepicker JavaScript -->
-		<script type="text/javascript" src="../vendors/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-		
-		<!-- Slimscroll JavaScript -->
-		<script src="dist/js/jquery.slimscroll.js"></script>
-	
-		<!-- Fancy Dropdown JS -->
-		<script src="dist/js/dropdown-bootstrap-extended.js"></script>
-		
-		<!-- Owl JavaScript -->
-		<script src="../vendors/bower_components/owl.carousel/dist/owl.carousel.min.js"></script>
-	
-		<!-- Init JavaScript -->
-		<script src="dist/js/init.js"></script>
-		
-		<script>
+<script>
 $(function(){
-$('.productId').on('change',function(e){
-e.preventDefault();
-var mainProduct = $(this).val()
-$.ajax({
-type:"POST",
-url: "../api/functions.php",
-data: {
-productValue: mainProduct,
-},
-success:function(result){
-$('.sizes').html(result);
-}
-});
-});
+	$('.productId').on('change',function(e){
+		e.preventDefault();
+		var mainProduct = $(this).val()
+			$.ajax({
+			type:"POST",
+			url: "../api/functions.php",
+			data: {
+				productValue: mainProduct,
+			},
+			success:function(result){
+				$('.sizes').html(result);
+			}
+		});
+	});
 })
 
 $(function(){
 $('.printMeNow').on('click',function(e){
-e.preventDefault();
-w = window.open();
-w.document.write($('.printable').html());
-w.print();
-w.close();
-});
+	e.preventDefault();
+	w = window.open();
+	w.document.write($('.printable').html());
+	w.print();
+	w.close();
+	});
 })
 
 </script>
-		
-	<!--///////////////////-->
 	
-		<script>
-        	 $(document).ready(function(){
-               $('#myAjaxTable').DataTable({
-                  'processing': true,
-                  'serverSide': true,
-                  'serverMethod': 'post',
-                  'order': [[0, 'desc']],
-                  'ajax': {
-                      'url':'template/ajax/reportAjax.php'
-                  },
-                  'columns': [
-                     { data: 'sl'},
-                     { data: 'date'},
-                     { data: 'ref'},
-                     { data: 'title'},
-                     { data: 'details'},
-                     { data: 'type'},
-                     { data: 'total'},
-                     { data: 'action' }
-                  ]
-               });
-            });
-		</script>
-	</body>
-</html>
+<script>
+$(document).ready(function(){
+	$('#myAjaxTable').DataTable({
+		'processing': true,
+		'serverSide': true,
+		'serverMethod': 'post',
+		'order': [[0, 'desc']],
+		'ajax': {
+			'url':'template/ajax/reportAjax.php'
+		},
+		'columns': [
+			{ data: 'sl'},
+			{ data: 'date'},
+			{ data: 'ref'},
+			{ data: 'title'},
+			{ data: 'details'},
+			{ data: 'type'},
+			{ data: 'total'},
+			{ data: 'action' }
+		]
+	});
+});
+</script>
