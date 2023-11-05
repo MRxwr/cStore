89474ab8a121f627de514a5b20e00e239d5f6acd
MRxwr <nasserhatab@gmail.com>
2023-11-06 01:42:43 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,8 @@
 <?php 
 require_once("template/header.php");
-
+if( isset($_GET["act"]) ){
+	die();
+}
 // get viewed page from pages folder \\
 if( isset($_GET["v"]) && searchFile("views","blade{$_GET["v"]}.php") ){
 	require_once("views/".searchFile("views","blade{$_GET["v"]}.php"));
