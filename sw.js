715cffa9aeab881196da46d29e37ff920c7d7a53
MRxwr <nasserhatab@gmail.com>
2024-04-16 02:12:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,27 @@
-var cacheName = 'YaZeenha';
+var cacheVersion = 'v1';
+var cacheName = 'YaZeenha-' + cacheVersion;
 var filesToCache = [
-    '/',
-    'index.php',
     'css/style.css',
     'js/main.js',
     'js/js.js'
 ];
+
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(filesToCache);
+        }).catch(function(err) {
+            console.error('Error caching files:', err);
         })
     );
 });
-/* Serve cached content when offline */
+
 self.addEventListener('fetch', function(e) {
-    e.respondWith(caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
-    }));
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        }).catch(function(err) {
+            console.error('Error fetching from cache:', err);
+        })
+    );
 });
