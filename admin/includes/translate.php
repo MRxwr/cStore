89b15fde9cac7308e050181e2c3c37f7f44b6efa
MRxwr <nasserhatab@gmail.com>
2024-04-16 03:21:04 +0300

    Update translate.php

--- admin/includes/translate.php
+++ admin/includes/translate.php
@@ -30,10 +30,10 @@ if ( isset($_GET["lang"]) ){
 	$arrayLangs = ["ENG","AR"];
 	if ( in_array($_GET["lang"], $arrayLangs) ){
 		setcookie("CREATEkwLANG","{$_GET["lang"]}",(86400*30) + time(), "/");
-		header("Refresh:0 , url=" . str_replace("?lang={$_GET["lang"]}", "" ,str_replace("&lang={$_GET["lang"]}", "", $_SERVER['REQUEST_URI'])) );
+		header("Refresh:0 , url=" . str_replace("?lang={$_GET["lang"]}", "" ,str_replace("&lang={$_GET["lang"]}", "", $_SERVER['REQUEST_URI'])) );die();
 	}else{
 		setcookie("CREATEkwLANG","{$settingsLang}",(86400*30) + time(), "/");
-		header("Refresh:0 , url=" . str_replace("?lang={$settingsLang}", "" ,str_replace("&lang={$settingsLang}", "", $_SERVER['REQUEST_URI'])) );
+		header("Refresh:0 , url=" . str_replace("?lang={$settingsLang}", "" ,str_replace("&lang={$settingsLang}", "", $_SERVER['REQUEST_URI'])) );die();
 	}
 }elseif( !isset($_COOKIE["CREATEkwLANG"]) ){
 	$_COOKIE["CREATEkwLANG"] = $settingsLang;
