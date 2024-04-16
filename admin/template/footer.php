20230adf0ec219b65407d6c8c34d619c54c77d2b
MRxwr <nasserhatab@gmail.com>
2024-04-16 04:38:14 +0300

    jquery

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -10,8 +10,6 @@
         <!-- /Main Content -->
     </div>
     <!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-
     <script src="../vendors/bower_components/jquery/dist/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
--- admin/template/header.php
+++ admin/template/header.php
@@ -53,6 +53,8 @@ require ("payment.php");
 
 	<!-- Toast CSS -->
 	<link href="../vendors/bower_components/jquery-toast-plugin/dist/jquery.toast.min.css" rel="stylesheet" type="text/css">	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
 	<!-- Custom CSS -->
 	<link href="dist/css/style.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="../css/font-awesome.min.css">
