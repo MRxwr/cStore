474c11329778c0dcc16f0cf1b7947332a5fac2ea
MRxwr <nasserhatab@gmail.com>
2024-05-16 02:27:36 +0300

    Update checksouthead.php

--- admin/includes/checksouthead.php
+++ admin/includes/checksouthead.php
@@ -5,7 +5,7 @@ require ("functions.php");
 if ( isset($_COOKIE[$cookieSession."A"]) && !empty($_COOKIE[$cookieSession."A"]) ){
 	session_start ();
 	$svdva = $_COOKIE[$cookieSession."A"];
-	if ( $user = selectDB("employees","`keepMeAlive` LIKE '".$svdva."'") ){
+	if ( $user = selectDBNew("employees", [$svdva], "`keepMeAlive` LIKE ?", "")){
 		$userID = $user[0]["id"];
 		$email = $user[0]["email"];
 		$username = $user[0]["fullName"];
