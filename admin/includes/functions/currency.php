2b010fe910f995959347d828eebf8782cc525b05
MRxwr <nasserhatab@gmail.com>
2024-06-01 10:42:46 +0300

    Update currency.php

--- admin/includes/functions/currency.php
+++ admin/includes/functions/currency.php
@@ -71,7 +71,7 @@ function setCurr($data){
 // convert prices regarding selected currency \\
 function priceCurr($data){
 	$selectedCurr = selectedCurr();
-	if( $currency = selectDBNew("currency",[$selectedCurr],"`short` LIKE CONCAT('%', ?, '%')","") ){
+	if( $currency = selectDB("currency","`short` LIKE CONCAT('%', '{$selectedCurr}', '%')") ){
 		return $data * $currency[0]["yourValue"];
 	}else{
 		return $data;
