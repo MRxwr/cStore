e67c8ae7abfb3e9911fa14fac600ea208f0f0f77
MRxwr <nasserhatab@gmail.com>
2024-10-31 13:06:27 +0300

    Update system.php

--- admin/includes/functions/system.php
+++ admin/includes/functions/system.php
@@ -183,6 +183,7 @@ function showLogo(){
 
 // whatsapp notification \\
 function whatsappNoti($order){
+	GLOBAL $settingsTitle;
 	if( $whatsappNoti = selectDB("settings","`id` = '1'") ){
 		$whatsappNoti1 = json_decode($whatsappNoti[0]["whatsappNoti"],true);
 		if( $whatsappNoti1["status"] != 1 ){
@@ -197,10 +198,12 @@ function whatsappNoti($order){
 			$data["invoiceid"] = $order;
 			$data["invoice_name"] = "invoice-{$whatsappNoti1["name"]}-{$order}";
 			$data["invoice_url"] = getPDF($order);
+			$data["caption"] = urlencode("Hello {$settingsTitle}, You have a new order #{$order}. <br>This is an automated message to notify you when you get new orders, Courtesy of createkuwait. <br> Best Regards, <br> createkuwait.com");
 		}
 	}else{
 		$data = array();
 	}
+	/*
 	$curl = curl_init();
 	curl_setopt_array($curl, array(
 	  CURLOPT_URL => 'https://automate.createstore.link/api/whatsapp/send',
@@ -215,6 +218,21 @@ function whatsappNoti($order){
 	  CURLOPT_HTTPHEADER => array(
 		'Cookie: XSRF-TOKEN=eyJpdiI6Im5NMzk2M3B6bU92UzdmY2tCK3NCMXc9PSIsInZhbHVlIjoiQnJrRjEzTjVON1ZSVjN4ZEtwN1oveCtHTXhnWU5pRTFEVHpNeStzRDhmZE1rN1dFMjMzb3ZENFJ0OUVXbnBGSmRYRmdPYm9hTmlkQnMxVUxCRDROU1Q0TXQvdE5JRS9heENTOEI3dW9Gb2d0Y1hJVi9UMUVQWHRFOWRDaGRxdHQiLCJtYWMiOiI3YWNhNmNmZDk4YjNmZDM5ZjM1YjY4ZDI5ZDI4YWIxMTZjYTdmOGNhODQ1ZmQwNGUxYmRmNWY5Y2VmMDE1NTZhIiwidGFnIjoiIn0%3D; laravel_session=eyJpdiI6IlJHY2sxMnVrdFBUOUZlSlQ3UlhJN0E9PSIsInZhbHVlIjoiTmhCS3RUaFJMOTZxNEF3TFdieXM1YkF4RTVJTGx4WkVadURLSWhnOEV1c0E4Tm9ueEdaUlJzSkgycXgvbzVmMXg4aWE2RkduVEdZdWdDdGtXdkMvQUdaVEJjRDZ6QzlCYWo5MmpzTzdvcmZQaUVJVS84RkdFZnJZVU9tQ0NCUHUiLCJtYWMiOiIzN2VmNzE2ZWZlZjQ0NmFjOGUzMjlhYzYxNTVlNTg5MGZlYWQ3ZWYyMGY1ZmRhMDMwMzkzYzkwYzY3Y2E0MGExIiwidGFnIjoiIn0%3D'
 	  ),
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	return $response;
+	*/
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://api.ultramsg.com/instance98521/messages/document?token=3u12vzrfp246uria&to=+{$data["to"]}&document={$data["invoice_url"]}&filename={$data["invoice_name"]}&caption={$data["caption"]}",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => '',
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 0,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => 'POST',
 	));
 	$response = curl_exec($curl);
 	curl_close($curl);
