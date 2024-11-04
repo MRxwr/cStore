57cce63b893b4e7570aa8c341bf309a25a03a9d5
MRxwr <nasserhatab@gmail.com>
2024-11-05 02:57:08 +0300

    Update bladePosInventory.php

--- admin/views/bladePosInventory.php
+++ admin/views/bladePosInventory.php
@@ -80,18 +80,15 @@ if ( isset($_POST) && !empty($_POST) ){
         );
 		if( isset($_POST) && !empty($_POST) && $products = selectJoinDB("attributes_products",$joinTabel,"t.status = '0' ORDER BY t.id ASC") ){
 		    for( $i = 0; $i < sizeof($products); $i++ ){
-                if ( $orders = selectDB2("SUM(quantity) as quantity, SUM(productPrice) as productPrice","posorders","`productId` = '{$products[$i]["id"]}' AND `date` BETWEEN '{$_POST["startDate"]}' AND '{$_POST["endDate"]}' AND `status` = '1' AND `shopId` = '{$_POST["shopId"]}'") ){
+                if ( $orders = selectDB2("SUM(quantity) as quantity","posorders","`productId` = '{$products[$i]["id"]}' AND `date` BETWEEN '{$_POST["startDate"]}' AND '{$_POST["endDate"]}' AND `status` = '1' AND `shopId` = '{$_POST["shopId"]}'") ){
                     $quantity = (empty($orders[0]["quantity"])) ? 0 : $orders[0]["quantity"];
-                    $productPrice = (empty($orders[0]["productPrice"])) ? 0 : $orders[0]["productPrice"];
                 }else{
                     $quantity = 0;
-                    $productPrice = 0;
                 }
             ?>
                 <tr>
                 <td><?php echo $products[$i]["pTitle"] . " " . $products[$i]["title"] ?></td>
                 <td><?php echo $quantity ?></td>
-                <td><?php echo $productPrice ?></td>
                 </tr>
             <?php
 		    }
