8779cff481d1366bf54a968d015b318f683ee219
MRxwr <nasserhatab@gmail.com>
2025-02-11 22:18:18 +0300

    Update bladeMaintenance.php

--- admin/views/bladeMaintenance.php
+++ admin/views/bladeMaintenance.php
@@ -516,7 +516,7 @@ $row = $result->fetch_assoc();
 		<div class="panel panel-default card-view">
 			<div class="panel-heading">
 				<div class="pull-left">
-					<h6 class="panel-title txt-dark"><?php echo direction("About US","معلومات عنا") ?></h6>
+					<h6 class="panel-title txt-dark"><?php echo direction("About US English","معلومات عنا الانجليزية") ?></h6>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -545,7 +545,7 @@ $row = $result->fetch_assoc();
 				<div class="panel-body">
 					<form method="POST" action="">
 					<div class="row">
-						<div class="col-md-6"><textarea id="arAbout" name="arAbout" class="tinymce"><?php echo $arAbout ?></textarea></div>
+						<div class="col-md-12"><textarea id="arAbout" name="arAbout" class="tinymce"><?php echo $arAbout ?></textarea></div>
 					</div>
 					<input type="submit" class="form-control btn btn-default"  value="submit">
 					</form>
