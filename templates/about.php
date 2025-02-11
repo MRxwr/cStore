d236c1d563882d0d31eb9682c7c7c5950a875c07
MRxwr <nasserhatab@gmail.com>
2025-02-11 22:30:42 +0300

    Update about.php

--- templates/about.php
+++ templates/about.php
@@ -2,7 +2,15 @@
 <div class="container-fluid">
 <div class="row d-flex justify-content-center">
 <div class="col-md-6">
-    <?php echo direction($about[0]["enAbout"], $about[0]["arAbout"]); ?>
+    <?php 
+        if( empty($about[0]["enAbout"]) ){
+            echo "{$about[0]["arTitle"]}";
+        }elseif( empty($about[0]["arAbout"]) ){
+            echo "{$about[0]["enTitle"]}";
+        }else{
+            echo direction($about[0]["enTitle"], $about[0]["arTitle"]);
+        }
+    ?>
 </div>
 </div>
 </div>
