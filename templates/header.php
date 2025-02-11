feb28edd65851d27121a3b61e165044e7071c310
MRxwr <nasserhatab@gmail.com>
2025-02-11 22:20:50 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -258,6 +258,13 @@ $fontImport = direction("@import url('https://fonts.googleapis.com/css2?family=S
 									}
 								}
 							}
+							if( isset($about) AND (!empty($about[0]["enAbout"]) || !empty($about[0]["arAbout"])) ){
+								echo "<li style='padding: 10px;'>
+											<a style='font-size: 20px;height: 36px;width: 36px;' href='about.php'>
+												<span class='fa fa-info' style='height: 15px; background: {$websiteColor}'></span>
+											</a>
+										</li>";
+							}
 							?>
 							</ul>
 						  </li>
