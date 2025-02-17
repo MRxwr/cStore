571613cc87fc5b2cdab72af58554c223dfe12c3e
MRxwr <nasserhatab@gmail.com>
2025-02-18 00:22:53 +0300

    save

--- admin/views/bladeInvoices.php
+++ admin/views/bladeInvoices.php
@@ -29,6 +29,8 @@ if( isset($_GET["type"]) && in_array($_GET["type"],$array) ){
 <div class="panel-body row">
 <div class="table-wrap">
 <div class="table-responsive">
+<input type="text" id="searchInput" placeholder="Search...">
+<div id="loading" style="display:none;">Loading...</div>
 <table class="table display responsive product-overview display dataTable mb-30" id="myTable">
 <thead>
 <tr>
@@ -42,59 +44,14 @@ if( isset($_GET["type"]) && in_array($_GET["type"],$array) ){
 </tr>
 </thead>
 <tbody>
-<?php
-if( $orders = selectDB("orders2","`id` != 0 {$type} GROUP BY `orderId` ORDER BY `date` DESC") ){
-    if( $paymentMethod = selectDB("p_methods","`id` != '0'") ){
-        for( $i = 0; $i < sizeof($paymentMethod); $i++ ){
-            $paymentMethodTitle[] = direction($paymentMethod[$i]["enTitle"],$paymentMethod[$i]["arTitle"]);
-        }
-    }
-    $statusId = [0,1,2,3,4,5,6];
-    $statusText = [direction("Pending","انتظار"),direction("Success","ناجح"),direction("Preparing","جاري التجهيز"), direction("On Delivery","جاري التوصيل"), direction("Delivered","تم تسليمها"), direction("Failed","فاشلة"),direction("Returned","مسترجعه")];
-    $statusBgColor = ["default","primary","info","warning","success","danger","default"];
-    for( $i = 0; $i < sizeof($orders); $i++ ){
-        $counter = $i + 1;
-        $info = json_decode($orders[$i]["info"],true);
-		$phone = $info["phone"];
-        $price = numTo3Float($orders[$i]["price"]+getExtrasOrder($orders[$i]["id"]));
-		$method = ( in_array($orders[$i]["paymentMethod"],array_keys($paymentMethod)) ) ? direction($paymentMethodTitle[$orders[$i]["paymentMethod"]],$paymentMethodTitle[$orders[$i]["paymentMethod"]]) : "";
-        $status="<div class='bg-{$statusBgColor[$orders[$i]["status"]]}' style='font-weight:700; color:black; padding:20px 15px;'>{$statusText[$orders[$i]["status"]]}</div>";
-        ?>
-        <tr>
-        <td><?php echo substr($orders[$i]["date"],0,10); ?></td>
-        <td><?php echo $orders[$i]["id"] ?></td>
-        <td><?php echo $phone ?></td>
-        <td><?php echo $price . " KD" ?></td>
-        <td><?php echo $method ?></td>
-        <td><?php echo $status ?></td>
-        <td>
-        <div class="btn-group">
-        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fa fa-cog"></i>
-        </button>   
-        <ul class="dropdown-menu">
-        <li><a href="javascript:void(0)" class="printNow" id="<?php echo $orders[$i]["id"] ?>"><i class="fa fa-print"></i> <?php echo direction("Print","طباعة") ?></a></li>
-        <li><a href="?v=Order&orderId=<?php echo $orders[$i]["id"] ?>"><i class="fa fa-eye"></i> <?php echo direction("View","عرض") ?></a></li>
-        <li><a href="<?php echo "?v={$_GET["v"]}&orderId={$orders[$i]["id"]}&status=1" ?>"><i class="fa fa-money"></i> <?php echo direction("Paid","مدفوعه") ?></a></li>
-        <li><a href="<?php echo "?v={$_GET["v"]}&orderId={$orders[$i]["id"]}&status=2" ?>"><i class="fa fa-clock-o"></i> <?php echo direction("Preparing","جاري التجهيز") ?></a></li>
-        <li><a href="<?php echo "?v={$_GET["v"]}&orderId={$orders[$i]["id"]}&status=3" ?>"><i class="fa fa-car"></i> <?php echo direction("On Delivery","جاري التوصيل") ?></a></li>
-        <li><a href="<?php echo "?v={$_GET["v"]}&orderId={$orders[$i]["id"]}&status=4" ?>"><i class="fa fa-car"></i> <?php echo direction("Delivered","تم التوصيل") ?></a></li>
-        <li><a href="<?php echo "?v={$_GET["v"]}&orderId={$orders[$i]["id"]}&status=5" ?>"><i class="fa fa-times"></i> <?php echo direction("Cancel","ملغية") ?></a></li>
-        <li><a href="<?php echo "?v={$_GET["v"]}&orderId={$orders[$i]["id"]}&status=6" ?>"><i class="fa fa-retweet"></i> <?php echo direction("Return","مسترجع") ?></a></li>
-        </ul>
-        </div>
-        </td>
-        </tr>
-        <?php
-    }
-}
-?>
-	
+<!-- Data will be populated here by AJAX -->
 </tbody>
 </table>
 </div>
-</div>	
-</div>	
+</div>
+<button id="prevPage">Previous</button>
+<button id="nextPage">Next</button>
+</div>
 </div>
 </div>
 </div>
@@ -143,36 +100,61 @@ $(function(){
         .appendTo("body");
 	});
 })
-</script>
-<?php
-/*
-<link href='https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
 
-<!-- Datatable JS -->
-<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-<script>
-$(document).ready(function(){
-   $('#AjaxTable').DataTable({
-      'processing': true,
-      'serverSide': true,
-      "pageLength": 25,
-      'serverMethod': 'post',
-      'ajax': {
-          'url':'../api/getInvoiceItems.php?v=<?=$_GET["v"]?>&type=<?=$tp?>'
-      },
-      'order': [[0, 'desc']],
-      'columns': [
-         { data: 'date' },
-         { data: 'orderId' },
-         { data: 'phone' },
-         { data: 'voucher' },
-         { data: 'price' },
-         { data: 'method' },
-         { data: 'status' },
-         { data: 'action' },
-      ]
-   });
+$(document).ready(function() {
+    function fetchData(start, length, search) {
+        $('#loading').show();
+        $.ajax({
+            url: '../api/fetchData.php',
+            type: 'GET',
+            data: {
+                start: start,
+                length: length,
+                search: search
+            },
+            success: function(response) {
+                var data = response.data;
+                var tableBody = $('#myTable tbody');
+                tableBody.empty();
+                for (var i = 0; i < data.length; i++) {
+                    var row = '<tr>' +
+                        '<td>' + data[i].date + '</td>' +
+                        '<td>' + data[i].orderId + '</td>' +
+                        '<td>' + data[i].phone + '</td>' +
+                        '<td>' + data[i].price + '</td>' +
+                        '<td>' + data[i].paymentMethod + '</td>' +
+                        '<td>' + data[i].status + '</td>' +
+                        '<td>' + data[i].actions + '</td>' +
+                        '</tr>';
+                    tableBody.append(row);
+                }
+                $('#loading').hide();
+            }
+        });
+    }
+
+    var start = 0;
+    var length = 10;
+    var search = '';
+
+    fetchData(start, length, search);
+
+    $('#nextPage').on('click', function() {
+        start += length;
+        fetchData(start, length, search);
+    });
+
+    $('#prevPage').on('click', function() {
+        if (start > 0) {
+            start -= length;
+            fetchData(start, length, search);
+        }
+    });
+
+    $('#searchInput').on('keyup', function() {
+        search = $(this).val();
+        start = 0;
+        fetchData(start, length, search);
+    });
 });
 </script>
-*/
-?>
